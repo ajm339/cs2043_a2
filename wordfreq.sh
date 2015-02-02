f11ac149fa15e0742f2d4827bc2af9d831812217
Alex Meyers <ajm339@cornell.edu>
2015-02-02 17:06:12 -0500

    yea

--- wordfreq.sh
+++ wordfreq.sh
@@ -1,11 +1,45 @@
 #! / bin / bash           
-min_words = 100
-max_words = 0
+min_words=100
+min_tweet="min_tweet"
+max_words=0
+max_tweet="max_tweet"
 
 for file in tweets/*.txt; do
+  count=0
 
   while read word; do
-    echo $word + "\n"
+    ((count++))
   done < $file
 
 done
+
+echo "DONE=============="
+echo $min_words
+echo $min_tweet
+echo $max_words
+echo $max_tweet
+
+function check_max{
+  new_max_found=false
+  count=$1
+  file_name=$2
+  if [$count=>max_words]; then
+    $max_words=$count
+    $max_tweet=$file_name
+    $new_max_found=true
+  fi
+
+  return $new_max_found
+}
+
+function check_min{
+  new_min_found=false
+  count=$1
+  file_name=$2
+  if [$count=>max_words]; then
+    $min_words=$count
+    $min_tweet=$file_name
+    $new_min_found=true
+  fi
+  return $new_min_found
+}
